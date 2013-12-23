715f4794206f7fcb1194224a7eb6e3a96fc87f25
Kristofor Vokes <kristofor@mym.ac>
2013-12-23 00:09:56 -0600

    added documentation to Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,18 @@
 class Item < ActiveRecord::Base
+  #The item class contains individual entries in a list. An Item belongs to a single List. 
+  #Below is a description of the model objects
+  # quantity: :default of 1 It is the obvious requested quantity of an item desired
+ #  item_name: Name of the item
+ #  approximate_cost: the cost of an individual item
+ #  source: where the item may be acquired
+ #  source_url: if the source is online, a link to the product
+ #  reason_for_item: why it is on the list
+ #  received: if the item has been acquired
+  
   belongs_to :list
+  
+  validates_presence_of (:item_name), :message => "Missing item name"
+  
+  
   
 end
