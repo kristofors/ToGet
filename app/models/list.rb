28aa3e0a3f94445092fa3be4f9115c502dabfdf3
Kristofor Vokes <kristofor@mym.ac>
2013-12-23 00:10:56 -0600

    destroy dependents, truncate name

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,22 @@
 class List < ActiveRecord::Base
-  has_many :items
+  has_many :items, dependent: :destroy
+  
+  validates_presence_of :list_name, :message => "can't be blank"
+  
+  before_validation :trim_name
   
   
+  
+  private
+  
+  def trim_name
+    # This method will work on a non-null list_name. If the text is over 32 characters, the text will be truncated.
+    if !self.list_name.nil?
+      if self.list_name.length > 31
+        self.list_name = self.list_name[0,31] + "…"
+      else
+        self.list_name = self.list_name[0,32]
+      end
+    end
+  end
 end
